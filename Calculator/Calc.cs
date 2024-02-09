1368a5d9abedf62b35486fa39b83ce2601744be5
Jan Carlo Juab <juabjancarlo@gmail.com>
2024-02-10 01:53:18 +0800

    Minor bug fix

--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -1,54 +1,25 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using static System.Collections.Specialized.BitVector32;
-
-namespace Calculator
+﻿namespace Calculator
 {
     internal class Calc
     {
-        public string? result;
-        private double ret = 0;
-        public void testConsole()
+        public double Sum(double x, double y)
         {
-            Console.WriteLine("TESTING");
+            return x + y;
         }
 
-        public double sum(double x, double y)
+        public double Difference(double x, double y)
         {
-            ret = x + y;
-            result = ret.ToString("F2");
-            return ret;
-        }
-        
-        public double product(double x, double y)
-        {
-            ret = x * y;
-            result = ret.ToString("F2");
-            return ret;
+            return x - y;
         }
 
-        public double quotient(double x, double y)
+        public double Product(double x, double y)
         {
-            ret = x * y;
-            result = ret.ToString("F2");
-            return ret;
+            return x * y;
         }
 
-        public double difference (double x, double y)
+        public double Quotient(double x, double y)
         {
-            ret = x - y;
-            result = ret.ToString("F2");
-            return ret;
+            return x / y;
         }
-
-        // public string Result() 
-        // {
-        //     if(this.result != null)
-        //     {
-        //         return this.result;
-        //     }
-        //     return "Result is null";
-        // }
-
     }
 }

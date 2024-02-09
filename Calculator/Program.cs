a99623b21bac4c5fea69506499dad9b08019d6ed
Jan Carlo Juab <juabjancarlo@gmail.com>
2024-02-10 00:39:13 +0800

    First commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,87 +1,62 @@
-﻿
+﻿namespace Calculator;
 
-namespace Calculator
+class Program
 {
-    internal class Program
+    static int Main(string[] args)
     {
-        static void Main(string[] args)
+        int argc = args.Length;
 
+        if (argc == 0)
         {
-            
-            Calc compute = new Calc();
-            double[] values = new double[2];
-            double memory = 0;
-            bool temp = false;
-            int action;
-
-            do
+            Console.WriteLine("For more information, try 'help'");
+            return 1;
+        }
+        else if (argc > 3)
+        {
+            Console.WriteLine("Too many arguments");
+            return 1;
+        }
+        else if (argc == 1)
+        {
+            if (args[0] == "help")
             {
-
-                if(memory != 0)
-                {
-                    Console.WriteLine("\nUse last calculation's result?\nENTER IF NO, TYPE ANYTHING FOR YES.\n");
-                    string input = Console.ReadLine() ?? "";
-                    if (!string.IsNullOrEmpty(input))
-                    {
-                        temp = true;
-                        values[0] = memory;
-                    }
-                }
-
-                if (!temp)
-                {
-                    Console.Write("Choose 1st Operand: ");
-                    while (!double.TryParse(Console.ReadLine(), out values[0]))
-                    {
-                        Console.Write("Invalid operand, choose a valid number: ");
-                    };
-                }
-
-                Console.Write("Choose 2nd Operand: ");
-                while (!double.TryParse(Console.ReadLine(), out values[1]))
-                {
-                    Console.Write("Invalid operand, choose a valid number: ");
-                }
-
-                Console.WriteLine("\nOperations:");
-                Console.WriteLine("1.) Addition");
-                Console.WriteLine("2.) Subtraction");
-                Console.WriteLine("3.) Multiplication");
-                Console.WriteLine("4.) Division");
-                Console.WriteLine("Invalid operation will restart the app.");
-                Console.Write("\nChoice: ");
-                while(!int.TryParse(Console.ReadLine(), out action))
-                {
-                    Console.Write("Enter a valid number: ");
-                }
-
-                switch (action)
-                {
-                    case 1:
-                        memory = compute.sum(values[0], values[1]);
-                        Console.WriteLine("Result: " + memory);
-                        break;
-                    case 2:
-                        memory = compute.difference(values[0], values[1]);
-                        Console.WriteLine("Result: " + memory);
-                        break;
-                    case 3:
-                        memory = compute.product(values[0], values[1]);
-                        Console.WriteLine("Result: " + memory);
-                        break;
-                    case 4:
-                        memory = compute.quotient(values[0], values[1]);
-                        Console.WriteLine("Result: " + memory);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid operation!\n"); break;
-                }
-
-                temp = false;
-
-            } while(true);
-
+                Console.WriteLine("Work in progress...");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("For more information, try 'help'");
+                return 1;
+            }
         }
 
+        int left = Convert.ToInt32(args[0]);
+        int right = Convert.ToInt32(args[2]);
+        int result = 0;
+
+        switch (args[1])
+        {
+            case "+":
+                result = left + right;
+                Console.Write(result);
+                break;
+            case "-":
+                result = left - right;
+                Console.Write(result);
+                break;
+            case "*":
+                result = left * right;
+                Console.Write(result);
+                break;
+            case "/":
+                result = left / right;
+                Console.Write(result);
+                break;
+            default:
+                Console.WriteLine("For more information, try 'help'");
+                return 1;
+        }
+
+        return 0;
     }
 }
